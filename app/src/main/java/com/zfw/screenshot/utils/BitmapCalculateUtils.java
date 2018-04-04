cc392771813c86fa96a86f4197a7e9564aa65c0c
DESKTOP-QQM4LP4\zengfw <cengfanwei@gmail.com>
2018-04-04 17:47:15 +0800

    20180404 17:47

--- app/src/main/java/com/zfw/screenshot/utils/BitmapCalculateUtils.java
+++ app/src/main/java/com/zfw/screenshot/utils/BitmapCalculateUtils.java
@@ -4,6 +4,42 @@ import android.graphics.Bitmap;
 
 public class BitmapCalculateUtils {
 
+    public static int calculateSamePart(Bitmap bm1, Bitmap bm2) {
+        int targetH = 100;
+        int len = bm1.getWidth();
+        int h1 = bm1.getHeight();
+        int h2 = bm2.getHeight();
+        int[] pixel_1 = new int[len];
+        int[] pixel_2 = new int[len];
+
+        int y1 = h1 - 1;
+        int y2 = h2 - 1;
+        int currY = -1;
+
+        bm1.getPixels(pixel_1, 0, len, 0, y1, len, 1);
+
+        for(; y2 > 0; y2--) {
+            bm2.getPixels(pixel_2, 0, len, 0, y2, len, 1);
+            if(isSameRow(pixel_1, pixel_2, len)) {
+                for(int i = 0; i < targetH; i++) {
+                    y1--;
+                    y2--;
+                    bm1.getPixels(pixel_1, 0, len, 0, y1, len, 1);
+                    bm2.getPixels(pixel_2, 0, len, 0, y2, len, 1);
+                    if(!isSameRow(pixel_1, pixel_2, len)) {
+                        y1 = h1 - 1;
+                        bm1.getPixels(pixel_1, 0, len, 0, y1, len, 1);
+                        break;
+                    }
+                }
+                return currY;
+            } else {
+                currY = y2 - 1;
+            }
+        }
+        return currY;
+    }
+
     /**
      * 3.非理想状态的算法：求出相似部分的高度
      * 这个算法有问题
