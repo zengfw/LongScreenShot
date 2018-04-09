c574c193be0c454b39a863fdaff158be55ea7f55
DESKTOP-QQM4LP4\zengfw <cengfanwei@gmail.com>
2018-04-09 14:47:06 +0800

    20180409 14:46

--- app/src/main/java/com/zfw/screenshot/adapter/ImageListAdapter.java
+++ app/src/main/java/com/zfw/screenshot/adapter/ImageListAdapter.java
@@ -2,6 +2,8 @@ package com.zfw.screenshot.adapter;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
+import android.support.v4.content.FileProvider;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -64,9 +66,15 @@ public class ImageListAdapter extends BaseAdapter {
         convertView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent localIntent = new Intent();
-                localIntent.setAction("android.intent.action.VIEW");
-                localIntent.setDataAndType(Uri.fromFile(new File(filePath)), "image/*");
+                Intent localIntent = new Intent(Intent.ACTION_VIEW);
+                localIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
+                    localIntent.setDataAndType(Uri.fromFile(new File(filePath)), "image/*");
+                } else {
+                    //通过FileProvider创建一个content类型的Uri(android 7.0需要这样的方法跨应用访问)
+                    Uri uri = FileProvider.getUriForFile(viewGroup.getContext(), viewGroup.getContext().getPackageName() + ".provider", new File(filePath));
+                    localIntent.setDataAndType(uri, "image/*");
+                }
                 viewGroup.getContext().startActivity(localIntent);
             }
         });
